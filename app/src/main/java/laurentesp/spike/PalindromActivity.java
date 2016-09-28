f26b31e1d98eab7fdd00c248a195404d182d1424
Laurent34 <laurent.esperance@gmail.com>
2016-09-28 13:44:07 +0200

    Add UI to Activity Palindrom

--- app/src/main/java/laurentesp/spike/PalindromActivity.java
+++ app/src/main/java/laurentesp/spike/PalindromActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PalindromActivity extends AppCompatActivity {
 
@@ -22,6 +25,16 @@ public class PalindromActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        final TextView textViewPal = (TextView) findViewById(R.id.text_view_pal_0);
+        final EditText editTextPal =(EditText) findViewById(R.id.edit_text_pal_0);
+        Button buttonEnter = (Button) findViewById(R.id.button_pal_0);
+
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewPal.setText(Palindrom.checkIfPalindrom(editTextPal.getText().toString()));
             }
         });
     }
