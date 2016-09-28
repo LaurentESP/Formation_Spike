2694798468556b5ff77a93ee26e5f39d6a41fd6b
Laurent34 <laurent.esperance@gmail.com>
2016-09-28 11:26:47 +0200

    Add Unit test for checkPalindrom

--- app/src/test/java/laurentesp/spike/ExampleUnitTest.java
+++ app/src/test/java/laurentesp/spike/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void shouldReturnReverseStringIfPalindrom() throws Exception {
+        assertEquals("lavaL", new Palindrom.checkIfPalindrom("Laval"));
+    }
 }
