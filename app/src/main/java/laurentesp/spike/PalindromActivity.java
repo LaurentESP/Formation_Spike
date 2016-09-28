ce52092c906aec5c9f64f44eb8481e1e51d00fc7
Laurent34 <laurent.esperance@gmail.com>
2016-09-28 16:54:15 +0200

    Modification of Palindrom Activity with KeyListener and no more Button

--- app/src/main/java/laurentesp/spike/PalindromActivity.java
+++ app/src/main/java/laurentesp/spike/PalindromActivity.java
@@ -1,10 +1,15 @@
 package laurentesp.spike;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +24,9 @@ public class PalindromActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,15 +35,30 @@ public class PalindromActivity extends AppCompatActivity {
             }
         });
         final TextView textViewPal = (TextView) findViewById(R.id.text_view_pal_0);
-        final EditText editTextPal =(EditText) findViewById(R.id.edit_text_pal_0);
-        Button buttonEnter = (Button) findViewById(R.id.button_pal_0);
+        final EditText editTextPal = (EditText) findViewById(R.id.edit_text_pal_0);
 
-        buttonEnter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textViewPal.setText(Palindrom.checkIfPalindrom(editTextPal.getText().toString()));
-            }
-        });
+        editTextPal.addTextChangedListener(new TextWatcher() {
+                                               public void afterTextChanged(Editable s) {
+
+                                               }
+
+                                               public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        /*This method is called to notify you that, within s, the count characters beginning at start are about to be replaced by new text with length after. It is an error to attempt to make changes to s from this callback.*/
+                                               }
+
+                                               public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                                   String getStringFromUser = editTextPal.getText().toString();
+                                                   if (Palindrom.isPalindrome(getStringFromUser)) {
+                                                       //fab.setBackgroundColor(Color.GREEN);
+                                                       fab.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                                                   } else {
+                                                       fab.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                                                   }
+                                                   textViewPal.setText(Palindrom.checkIfPalindrom(getStringFromUser));
+                                               }
+                                           }
+
+        );
     }
 
 }
